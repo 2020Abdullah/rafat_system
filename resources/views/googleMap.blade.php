33c9991a11567d6d7d0273b7c47599185eef760e
Anas Rabea <anas.rabea100@gmail.com>
2023-04-10 20:41:46 +0200

    form

--- resources/views/googleMap.blade.php
+++ resources/views/googleMap.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" lang="ar" dir="rtl">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -33,8 +33,11 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-12 mb-3">
+                            <div class="d-flex justify-content-center">
+                                <h2>تقرير الزيارة </h2>
+                            </div>
                             <button class="btn btn-success" id="location">
-                                get location
+                                الموقع الحالي
                             </button>
                         </div>
                         <div class="col-md-12">
@@ -42,10 +45,52 @@
                                 @csrf
                                 <input type="text" class="form-control" id="latitude" name="lat" readonly>
                                 <input type="text" class="form-control" id="longitude" name="long" readonly>
-                                <input type="submit" class="btn btn-danger" value="save" id="save">
+                                <input type="submit" class="btn btn-danger" value="حفظ" id="save">
+
                             </form>
                             <div class="output" style="width: 100%">
                             </div>
+                            <form>
+                                <div class="mb-3">
+                                    <label for="usernumber" class="form-label">رقم اليوزر </label>
+                                    <input type="number" class="form-control" id="usernumber">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="typePhone">رقم جوال صاحب اليوزر</label>
+                                    <input type="tel" id="typePhone" class="form-control" />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="usercount" class="form-label">كم الرصيد في اليوزر </label>
+                                    <input type="number" class="form-control" id="usercount">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="usercard" class="form-label">كم عدد الشرايح الجديده</label>
+                                    <input type="number" class="form-control" id="usercard">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="activenumber" class="form-label">كم عدد التفعيلات </label>
+                                    <input type="number" class="form-control" id="activenumber">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="notes" class="form-label">ملاحظات عن السوق و حالة اليوزر</label>
+                                    <input type="text" class="form-control" id="notes">
+                                </div>
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>اسم المشرف</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                                <br/>
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>اسم المطور</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                                <br/>
+                                <button type="submit" class="btn btn-primary form-control">إرسال</button>
+                                </form>
                         </div>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('test');
+    return view('googleMap');
 });
 
 Route::post('/save', function (Request $request) {

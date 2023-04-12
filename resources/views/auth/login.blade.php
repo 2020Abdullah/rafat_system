1d187f13fe65d7f84fc825e6ca525bdca1e8cff4
Anas Rabea <anas.rabea100@gmail.com>
2023-04-12 23:22:07 +0200

    loginform

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,185 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+:root {
+    --primary-color: #ff5722;
+    --secondary-color: #ff5722;
+    --tertiary-color: #673ab7;
+    --gray-color: #b0b0b0;
+}
+
+* {
+    box-sizing: border-box;
+    font-family: 'Raleway', sans-serif;
+    line-height: 1;
+    padding: 0;
+    margin: 0;
+}
+
+body {
+    font-family: 'Open Sans', sans-serif;
+    background:#e2e2e2;
+}
+
+
+input {
+  display: block;
+  height: 50px;
+  width: 90%;
+  margin: 0 auto;
+  border: none;
+}
+input::placeholder {
+  -webkit-transform: translateY(0px);
+  transform: translateY(0px);
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+}
+input:hover, input:focus, input:active:focus {
+  color: #ff5722;
+  outline: none;
+  border-bottom: 1px solid #ff5722;
+}
+input:hover::placeholder, input:focus::placeholder, input:active:focus::placeholder {
+  color: #ff5722;
+  position: relative;
+  -webkit-transform: translateY(-20px);
+  transform: translateY(-20px);
+}
+
+
+.form_container {
+    height: 80vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.email,
+.pwd {
+  position: relative;
+  z-index: 1;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  padding-left: 20px;
+  font-family: "Open Sans", sans-serif;
+  text-transform: uppercase;
+  color: #858585;
+  font-weight: lighter;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+}
+
+.box {
+    background-color: white;
+    border-radius: 10px;
+    padding: 45px;
+    width: 375px;
+    max-width: 95%;
+    box-shadow: 5px 5px 10px 1px rgb(0, 0, 0, 10%);
+}
+
+@media (max-width: 480px) {
+    .box {
+        padding: 75px 25px;
+    }
+}
+
+.box h1 {
+    font-size: 35px;
+    font-weight: 800;
+    text-align: center;
+    margin-bottom: 45px;
+    color: var(--tertiary-color);
+}
+
+.box form label {
+    display: block;
+    font-size: 12px;
+    margin-bottom: 3px;
+}
+
+.box form div {
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid var(--gray-color);
+}
+
+.box form div:hover {
+    border-bottom-color: var(--secondary-color);
+}
+
+.box form div:first-of-type {
+    margin-bottom: 35px;
+}
+
+.box form div i {
+    font-size: 15px;
+    padding-left: 10px;
+    color: var(--gray-color);
+}
+
+.box form div:hover i {
+    color: var(--secondary-color);
+}
+
+.box form div input {
+    font-size: 12px;
+    outline: none;
+    border: none;
+    padding: 10px;
+    min-width: 0;
+    flex: 1;
+}
+
+.box form div input::placeholder {
+    opacity: 1;
+    color: var(--gray-color);
+    font-size: 12px;
+}
+
+.box a {
+    color: var(--gray-color);
+    text-decoration: none;
+    font-size: 12px;
+    display: block;
+}
+
+.box a:hover {
+    color: var(--secondary-color);
+}
+
+.box form .forgot {
+    margin-top: 15px;
+    float: right;
+}
+
+.box form input[type="submit"] {
+    font-size: 12px;
+    font-weight: 600;
+    text-transform: uppercase;
+    background-color: var(--secondary-color);
+    color: white;
+    border: none;
+    width: 100%;
+    padding: 15px;
+    margin-top: 45px;
+    border-radius: 250px;
+}
+
+.box form input[type="submit"]:hover {
+    background-color: var(--tertiary-color);
+    cursor: pointer;
+}
+
+.box .sign-up {
+    margin-top: 25px;
+    text-align: center;
+    text-transform: uppercase;
+}
+</style>
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +247,24 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+
+
+    <div class="form_container">
+        <div class="box">
+            <h1>تسجيل الدخول</h1>
+            <form>
+                <div>
+                    <i class="fa-solid fa-user"></i>
+                    <input class="email" type="text" placeholder="ادخل الاميل">
+                </div>
+                <div>
+                    <i class="fa-solid fa-lock"></i>
+                    <input class="pwd" type="password" placeholder="ادخل كلمة السر">
+                </div>
+                <input type="submit" value="تسجيل">
+            </form>
+        </div>
+    </div>
 @endsection
